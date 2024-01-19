a04182e68ddf32f778e4ebeaca14b10ce961d51c
Rainy2514 <podolskiy2006@mail.ru>
2024-01-19 18:08:54 +0300

    Графики

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+public class Graph : MonoBehaviour
+{
+    [Range(10, 100)]
+    public int resolution = 10;
+    private int currentResolution;
+    private ParticleSystem.Particle[] points;
+    private void CreatePoints()
+    {
+        currentResolution = resolution;
+        points = new ParticleSystem.Particle[resolution];
+        float increment = 1f / (resolution - 1);
+        for (int i = 0; i < resolution; i++)
+        {
+            float x = i * increment;
+            points[i].position = new Vector3(x, 0f, 0f);
+            points[i].color = new Color(255, 0f, 0f);
+            points[i].size = 0.1f;
+        }
+    }
+    void Start()
+    {
+        if (resolution < 10 || resolution > 100) 
+    {
+            Debug.LogWarning("Разрешение графика вышло за границы, сброшено в минимум", this);
+            resolution = 10;
+        }
+        points = new ParticleSystem.Particle[resolution];
+        float increment = 1f / (resolution - 1);
+        for (int i = 0; i < resolution; i++)
+        {
+            float x = i * increment;
+            points[i].position = new Vector3(x, 0f, 0f);
+            points[i].color = new Color(255, 0f, 0f);
+            points[i].size = 0.1f;
+        }
+    }
+    void Update()
+    {
+        if (currentResolution != resolution)
+        {
+            CreatePoints();
+        }
+        for (int i = 0; i < resolution; i++)
+        {
+            Vector3 p = points[i].position;
+            p.y = p.x;
+            points[i].position = p;
+            Color c = points[i].color;
+            c.g = p.y;
+            points[i].color = c;
+        }
+        GetComponent<ParticleSystem>().SetParticles(points, points.Length);
+    }
+}
